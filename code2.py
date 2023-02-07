3c645206b2268f607b32a1ffb8dbdfeff63b68bb
smithy1711 <smithy1711@hotmail.com>
2023-02-07 14:00:12 +0000

    some attempt at parameter fitting

--- code2.py
+++ code2.py
@@ -1,8 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import odeint
+import pygad
+from scipy.optimize import minimize
+
 
 # Constants
+
+#to be determined by optimization
 kjs = 0.015
 kgj = 0.005
 kjl = 0.025
@@ -11,11 +16,12 @@ kxg = 0.0005
 fgj = 0.5
 kxi = 0.1
 kxgi = 0.0005
-s = 1
 
-Ib = 10
 beta = 0.5
 Y = 2
+
+# steady-state values
+Ib = 10
 Gb = 100
 
 l = 600 #cm, l is the length of the small intestine from the jejunum to the ilium, generally 600-700cm
@@ -31,11 +37,10 @@ n = 0.1 #mg/s, n is the rate of glucose absorption in the small intestine, gener
 S0 = 75
 J0 = 0.0
 L0 = 0.0
-G0 = 0.0
+G0 = 196
 G_ = 0.0
 I0 = 0.0
 
-#Gprod = 
 
 # Time points
 T = 500
@@ -61,68 +66,98 @@ I[0] = I0
 
 
 # Solve differential equations
-for i in range(1, len(time)):
-    dSdt = -kjs*S[i-1]
-    dJdt = kjs*S[i-1] - kgj*J[i-1] - kjl*J[i-1]
+def model():
+    for i in range(1, len(time)):
+        dSdt = -kjs*S[i-1]
+        dJdt = kjs*S[i-1] - kgj*J[i-1] - kjl*J[i-1]
 
-    dGdt = -kxg*G[i-1] - kxgi*G[i-1] + Gprod + n*(kgj*J[i-1] + kgl*L[i-1])
+        dGdt = -kxg*G[i-1] - kxgi*G[i-1]*I[i-1] + Gprod + n*(kgj*J[i-1] + kgl*L[i-1])
 
-    # Calculate phi at current time step
-    if time[i] < tau:
-        phi = 0
-    else:
-        phi = J[np.floor(i-tau).astype(int)]
-    # Calculate derivative at current time step
-    dLdt = kjl * phi - kgl * L[i-1]
-    
-    G_[i] = G[i-1] + np.sum(fgj * (kgj * J + kgl * L))
-    dIdt = kxi * Ib * ((beta**Y + 1) / ((beta**Y * (Gb/G_[i-1]))**Y + 1) - I[i-1]/Ib)
-    
+        # Calculate phi at current time step
+        if time[i] < tau:
+            phi = 0
+        else:
+            phi = J[np.floor(i-tau).astype(int)]
+        # Calculate derivative at current time step
+        dLdt = kjl * phi - kgl * L[i-1]
+        
+        G_[i] = G[i-1] + np.sum(fgj * (kgj * J + kgl * L))
+        dIdt = kxi * Ib * ((beta**Y + 1) / ((beta**Y * (Gb/G_[i-1]))**Y + 1) - I[i-1]/Ib)
+        
 
-    # Update solution at current time step
-    S[i] = S[i-1] + dSdt*dt
-    J[i] = J[i-1] + dJdt*dt
-    L[i] = L[i-1] + dLdt*dt
-    G[i] = G[i-1] + dGdt*dt
-    I[i] = I[i-1] + dIdt*dt
-
-# Plot solutions
-
-#Bolus value in the stomach over time
-plt.figure(figsize=(10, 5))
-plt.subplot(2, 3, 1)
-plt.plot(time, S, label='S(t)')
-plt.xlabel('Time (s)')
-plt.ylabel('S(t)')
-
-#Amount of glucose in the jejunum over time
-plt.subplot(2, 3, 2)
-plt.plot(time, J, label='J(t)')
-plt.xlabel('Time (s)')
-plt.ylabel('J(t)')
-
-#Amount of glucose in the ilium over time
-plt.subplot(2, 3, 3)
-plt.plot(time, L)
-plt.xlabel('Time (s)')
-plt.ylabel('L(t)')
-
-#Glucose concentration in the blood over time
-plt.subplot(2, 3, 4)
-plt.plot(time, G)
-plt.xlabel('Time (s)')
-plt.ylabel('G(t)')
-
-#Insulin concentration in the blood over time
-plt.subplot(2, 2, 4)
-plt.plot(time, I)
-plt.xlabel('Time (s)')
-plt.ylabel('I(t)')
+        # Update solution at current time step
+        S[i] = S[i-1] + dSdt*dt
+        J[i] = J[i-1] + dJdt*dt
+        L[i] = L[i-1] + dLdt*dt
+        G[i] = G[i-1] + dGdt*dt
+        I[i] = I[i-1] + dIdt*dt
 
 
-plt.show()
+    # Plot solutions
 
-values = np.hstack((S[:, np.newaxis], J[:, np.newaxis], L[:, np.newaxis], G[:, np.newaxis], I[:, np.newaxis]))
+    #Bolus value in the stomach over time
+    plt.figure(figsize=(10, 5))
+    plt.subplot(2, 3, 1)
+    plt.plot(time, S, label='S(t)')
+    plt.xlabel('Time (s)')
+    plt.ylabel('S(t)')
 
-# Save data to CSV files
-np.savetxt("values.csv", values, delimiter=",")
+    #Amount of glucose in the jejunum over time
+    plt.subplot(2, 3, 2)
+    plt.plot(time, J, label='J(t)')
+    plt.xlabel('Time (s)')
+    plt.ylabel('J(t)')
+
+    #Amount of glucose in the ilium over time
+    plt.subplot(2, 3, 3)
+    plt.plot(time, L)
+    plt.xlabel('Time (s)')
+    plt.ylabel('L(t)')
+
+    #Glucose concentration in the blood over time
+    plt.subplot(2, 3, 4)
+    plt.plot(time, G)
+    plt.xlabel('Time (s)')
+    plt.ylabel('G(t)')
+
+    #Insulin concentration in the blood over time
+    plt.subplot(2, 2, 4)
+    plt.plot(time, I)
+    plt.xlabel('Time (s)')
+    plt.ylabel('I(t)')
+
+
+    plt.show()
+
+    values = np.hstack((S[:, np.newaxis], J[:, np.newaxis], L[:, np.newaxis], G[:, np.newaxis], I[:, np.newaxis]))
+
+    # Save data to CSV files
+    np.savetxt("values.csv", values, delimiter=",")
+
+def cost_function(theta, alpha, Gexp, Iexp, Gmax, Gmin, Imax, Imin):
+    T = [0, 30, 60, 90, 120]
+    Gnum = solve_glucose_insulin_dynamics(theta)
+    Inum = solve_insulin_dynamics(theta)
+    J = 1 / (5 * (Gmax - Gmin) ** 2) * np.sum((Gexp - Gnum[T[i-1]]) ** 2)
+    J += alpha / (5 * (Imax - Imin) ** 2) * np.sum((Iexp - Inum[T[i-1]]) ** 2)
+    return J
+
+def solve_glucose_insulin_dynamics(theta):
+    # Solve the glucose dynamics
+    G = np.zeros_like(time)
+    G[0] = G0
+    for i in range(1, len(time)):
+        dGdt = -kxg*G[i-1] - kxgi*G[i-1] + Gprod + n*(kgj*J[i-1] + kgl*L[i-1])
+        G[i] = G[i-1] + dGdt*dt
+    return G
+
+def solve_insulin_dynamics(theta):
+    # Solve the insulin dynamics
+    I = np.zeros_like(time)
+    I[0] = I0
+    for i in range(1, len(time)):
+        G_[i] = G[i-1] + np.sum(fgj * (kgj * J + kgl * L))
+        dIdt = kxi * Ib * ((beta**Y + 1) / ((beta**Y * (Gb/G_[i-1]))**Y + 1) - I[i-1]/Ib)
+        I[i] = I[i-1] + dIdt*dt
+    return I
+
