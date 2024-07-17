dcc4dc7a1d3687171aab5f6cb584c7bd86c01736
bthawsmith <ben.smith@cityfootball.com>
2024-07-17 11:24:21 +0100

    Initial commit

--- parameterFitting.py
+++ parameterFitting.py
@@ -83,18 +83,17 @@ def Glucose_Insulin(kjs, kgj, kjl, kgl, kxi, kxg, kxgi, n, klambda, k2, x):
     #G_[0] = G0
     G = np.zeros_like(time)
     G[0] = G0
-    Gprod = np.zeros_like(time)
-    Gprod[0] = Gprod0
+    I = Insulin(kxi)
+    S = Stomach(kjs)
     J = Jejunum(kjs, kgj, kjl)
     L = Ileum(kjs, kgj, kjl, kgl)
-    S = Stomach(kjs)
-    I = Insulin(kxi)
+    Gprod = np.zeros_like(time)
 
 
     for i in range(1, len(time)):
         Gprod[i] = (klambda * (Gb-G[i-1])) / (k2 + (Gb - x)) + Gprod[0] 
         
-        #klambda - mM2 min−1 Kinetic constant for hepatic glucose release rate
+        '''#klambda - mM2 min−1 Kinetic constant for hepatic glucose release rate
 
         
         #G_[i] = G[i-1] + np.sum(fgj * (kgj * J[i-1] + kgl * L[i-1])) not needed in a person with type 1 diabetes
@@ -108,7 +107,7 @@ def Glucose_Insulin(kjs, kgj, kjl, kgl, kxi, kxg, kxgi, n, klambda, k2, x):
         # beta and gamma are not needed in a diabetic person because they are not producing insulin. beta and gamma are parameters for half saturation and acceleration of the insulin production 
         # insulin can take up to 30 mins before it starts to work. 1 to 3 hours for the peak activity. https://www.diabetes.co.uk/insulin/insulin-actions-and-durations.html
         # can replace the acceleration with a delay? also may need to account for basal insulin or "left over insulin" in the body
-        # dIdt = kxi * Ib * ((1) / (((Gb/G_[i-1])) + 1) - I[i-1]/Ib)
+        # dIdt = kxi * Ib * ((1) / (((Gb/G_[i-1])) + 1) - I[i-1]/Ib)'''
         if i < 30:
             dGdt = -kxg*G[i-1] + Gprod[i-1] + n*(kgj*J[i-1] + kgl*L[i-1])
         else:
@@ -116,6 +115,15 @@ def Glucose_Insulin(kjs, kgj, kjl, kgl, kxi, kxg, kxgi, n, klambda, k2, x):
         G[i] = G[i-1] + dGdt*dt # when dgdt*dt becomes <0 find value of dgdt. this gives natural loss of glucose in the blood.
 
     return S, G, I, J, L, Gprod
+
+dataS = np.loadtxt('Data/dataS.csv', delimiter=',')
+dataJ = np.loadtxt('Data/dataJ.csv', delimiter=',')
+dataL = np.loadtxt('Data/dataL.csv', delimiter=',')
+dataGprod = np.loadtxt('Data/dataGprod.csv', delimiter=',')
+dataI = np.loadtxt('Data/dataI.csv', delimiter=',')
+dataG = np.loadtxt('Data/dataG.csv', delimiter=',')
+
+
 
 #parameter fitting
 
@@ -128,14 +136,6 @@ def objectiveS(x, data):
     error = np.sum(np.square(S_ - data)) #mean squared error
     
     return error
-
-
-dataS = np.loadtxt('dataS.csv', delimiter=',')
-dataJ = np.loadtxt('dataJ.csv', delimiter=',')
-dataL = np.loadtxt('dataL.csv', delimiter=',')
-dataGprod = np.loadtxt('dataGprod.csv', delimiter=',')
-dataI = np.loadtxt('dataI.csv', delimiter=',')
-dataG = np.loadtxt('dataG.csv', delimiter=',')
 
 #stomach
 resultS = minimize(objectiveS, x0=0.05, args=(dataS,)) #used to minimize a scalar function of one or more variables
@@ -167,27 +167,24 @@ def objectiveI(x, data):
 resultI = minimize(objectiveI, x0=0.05, args=(dataI,)) #used to minimize a scalar function of one or more variables
 kxi = resultI.x[0]
 
-print(kjs, kxi)
-
 def error_function(params):
     # Get the model predictions using the current parameters
     S, G, I, J, L, Gprod = Glucose_Insulin_Fixed(*params)
 
     # Calculate the error (sum of squared differences) between the model predictions and the data
-    #error_S = np.sum((S - dataS)**2)
+    error_S = np.sum((S - dataS)**2)
+    print(error_S)
     error_G = np.sum((G - dataG)**2)
-    #error_I = np.sum((I - dataI)**2)
-    #error_J = np.sum((J - dataJ)**2)
-    #error_L = np.sum((L - dataL)**2)
-    #error_Gprod = np.sum((Gprod - dataGprod)**2)
+    error_I = np.sum((I - dataI)**2)
+    error_J = np.sum((J - dataJ)**2)
+    error_L = np.sum((L - dataL)**2)
+    error_Gprod = np.sum((Gprod - dataGprod)**2)
 
     # Return the total error
-    #return error_S + error_G + error_I + error_J + error_L + error_Gprod
-    return error_G
+    return error_S + error_G + error_I + error_J + error_L + error_Gprod
+    #return error_G
 
 def Glucose_Insulin_Fixed(kgj, kjl, kgl, kxg, kxgi, n, klambda, k2, x):
-    #G_ = np.zeros_like(time)
-    #G_[0] = G0
     G = np.zeros_like(time)
     G[0] = G0
     Gprod = np.zeros_like(time)
@@ -199,22 +196,6 @@ def Glucose_Insulin_Fixed(kgj, kjl, kgl, kxg, kxgi, n, klambda, k2, x):
 
     for i in range(1, len(time)):
         Gprod[i] = (klambda * (Gb-G[i-1])) / (k2 + (Gb - x)) + Gprod[0] 
-        
-        #klambda - mM2 min−1 Kinetic constant for hepatic glucose release rate
-
-        
-        #G_[i] = G[i-1] + np.sum(fgj * (kgj * J[i-1] + kgl * L[i-1])) not needed in a person with type 1 diabetes
-
-        
-        # when a value >0 is given for G0 then dGdt will give the natural loss of glucose.
-        # also making there be no bolus then J and L are 0. also I is 0 as there is no administration of insuline
-        # therefore dGdt is just -kxg*G[i-1]
-
-        # dIdt = kxi * Ib * ((beta**Y + 1) / ((beta**Y * (Gb/G_[i-1]))**Y + 1) - I[i-1]/Ib) #look at each part of the equation in the paper
-        # beta and gamma are not needed in a diabetic person because they are not producing insulin. beta and gamma are parameters for half saturation and acceleration of the insulin production 
-        # insulin can take up to 30 mins before it starts to work. 1 to 3 hours for the peak activity. https://www.diabetes.co.uk/insulin/insulin-actions-and-durations.html
-        # can replace the acceleration with a delay? also may need to account for basal insulin or "left over insulin" in the body
-        # dIdt = kxi * Ib * ((1) / (((Gb/G_[i-1])) + 1) - I[i-1]/Ib)
         if i < 30:
             dGdt = -kxg*G[i-1] + Gprod[i-1] + n*(kgj*J[i-1] + kgl*L[i-1])
         else:
@@ -229,11 +210,11 @@ bounds = [(0.01, 0.1), (0.004, 0.01), (0.01, 0.06), (0.01, 0.06), (0.01, 0.05), 
 
 # Use differential_evolution to find the best parameters
 result = differential_evolution(error_function, bounds, strategy='best1bin', popsize=50, mutation=(0.5, 1), recombination=0.7, tol=0.01)
+print(result)
 
 # Get the optimal parameters from the result
 optimal_params = result.x
 
-np.savetxt('dataOpt3.csv', optimal_params, delimiter=',')
 
 kgj = optimal_params[0]
 kjl = optimal_params[1]
@@ -249,57 +230,27 @@ IP = [0.034, 0.025, 0.067, 0.007, 0.02, 0.018, 0.028, 0.01, 0.6, 22, 16]
 print("Param | Init | Fitted \n ---------------- \n kjs | ", IP[0], " | ", kjs, " \n kxi | ", IP[1], " | ", kxi, " \n kgj | ", IP[2], " | ", kgj, " \n kjl | ", IP[3], " | ", kjl, " \n kgl | ", IP[4], " | ", kgl, " \n kxg | ", IP[5], " | ", kxg, " \n kxgi | ", IP[6], " | ", kxgi, " \n n | ", IP[7], " | ", n, " \n klambda | ", IP[8], " | ", klambda, " \n k2 | ", IP[9], " | ", k2, " \n x | ", IP[10], " | ", x, " \n ---------------- \n")
 
 
-#dataO = np.loadtxt('dataOpt3.csv', delimiter=',')
-#kgj, kjl, kgl, kxg, kxgi, n, klambda, k2, x = dataO[0], dataO[1], dataO[2], dataO[3], dataO[4], dataO[5], dataO[6], dataO[7], dataO[8]
-#optimal_params = [3.40021405e-02, 5.65384889e-02, 1.75511863e-02, 1.00000000e-01, 1.91951275e-02, 8.62602464e-02, 7.07340053e-02, 2.75809316e-02, 1.00000000e-01, 2.89764446e+01, 1.01013288e+01]
-
-# Print the optimal parameters
-#print("Optimal parameters:", optimal_params)
 
 # Run the simulation with the optimal parameters and compare the results with the data
 S_opt, G_opt, I_opt, J_opt, L_opt, Gprod_opt = Glucose_Insulin(kjs, kgj, kjl, kgl, kxi, kxg, kxgi, n, klambda, k2, x)
 
 
-print("done")
 
-plt.figure()
-plt.subplot(3,2,1)
-plt.plot(time, G_opt, label="Simulated Glucose")
-plt.plot(time, dataG, label="Experimental Glucose")
-plt.ylabel("Glucose Concentration")
-plt.legend()
+def plot_simulation_vs_experiment(time, sim_data, exp_data, labels, ylabels, xlabel=None):
+    plt.figure(figsize=(12, 12))
+    for i, (sim, exp, label, ylabel) in enumerate(zip(sim_data, exp_data, labels, ylabels), 1):
+        plt.subplot(3, 2, i)
+        plt.plot(time, sim, label=f"Simulated {label}")
+        plt.plot(time, exp, label=f"Experimental {label}")
+        plt.ylabel(ylabel)
+        plt.legend()
+        if i > 4:
+            plt.xlabel(xlabel)
+    plt.show()
 
-plt.subplot(3,2,2)
-plt.plot(time, I_opt, label="Simulated Insulin")
-plt.plot(time, dataI, label="Experimental Insulin")
-plt.ylabel("Insulin Concentration")
-plt.legend()
+simulated_data = [G_opt, I_opt, J_opt, L_opt, Gprod_opt, S_opt]
+experimental_data = [dataG, dataI, dataJ, dataL, dataGprod, dataS]
+labels = ["Glucose", "Insulin", "Jejunum", "Ileum", "Gprod", "Stomach"]
+ylabels = ["Glucose Concentration", "Insulin Concentration", "Jejunum Concentration", "Ileum Concentration", "Gprod Concentration", "Stomach Concentration"]
 
-plt.subplot(3,2,3)
-plt.plot(time, J_opt, label="Simulated Jejunum")
-plt.plot(time, dataJ, label="Experimental Jejunum")
-plt.ylabel("Jejunum Concentration")
-plt.legend()
-
-plt.subplot(3,2,4)
-plt.plot(time, L_opt, label="Simulated Ileum")
-plt.plot(time, dataL, label="Experimental Ileum")
-plt.ylabel("Ileum Concentration")
-plt.legend()
-
-plt.subplot(3,2,5)
-plt.plot(time, Gprod_opt, label="Simulated Gprod")
-plt.plot(time, dataGprod, label="Experimental Gprod")
-plt.xlabel("Time")
-plt.ylabel("Gprod Concentration")
-plt.legend()
-
-plt.subplot(3,2,6)
-plt.plot(time, S_opt, label="Simulated Stomach")
-plt.plot(time, dataS, label="Experimental Stomach")
-plt.xlabel("Time")
-plt.ylabel("Stomach Concentration")
-plt.legend()
-
-plt.show()
-
+plot_simulation_vs_experiment(time, simulated_data, experimental_data, labels, ylabels, xlabel="Time")
